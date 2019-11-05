020dfbfca00d4ca4aba1058d7cf0af798baee9dd
nangpham26 <54834355+nangpham26@users.noreply.github.com>
2019-11-05 17:31:02 +0700

    commit

---
+++ modules/10/10-content.php
@@ -0,0 +1,75 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div class="type-10">
+        <div class="container">
+            <h4 class="widget-title">Popular Blog Posts</h4>
+            <div class="row fix-row">
+                <div class="col-md-3 fix-size col-xs-3 col-sm-3">
+                    <img src="../10/images/2.png" alt="hinh" class="img-responsive">
+                </div>
+                <div class="col-md-9 fix-size1 col-xs-9 col-sm-9">
+                <span class="sub-title">How to Function at Work Withou…</span>
+                <div class="des">
+                Etiam placerat velit vitae dui blandit sollicitudin. Vestibulum
+                </div>
+                </div>
+                
+            </div>
+            <div class="row fix-row">
+                <div class="col-md-3 fix-size col-xs-3">
+                    <img src="../10/images/4.jpeg" alt="hinh" class="img-responsive">
+                </div>
+                <div class="col-md-9 fix-size1 col-xs-9">
+                <span class="sub-title">How to End Your Emails</span>
+                <div class="des">
+                Quis vulputate justo euismod ac. Nunc viverra sollicitudin
+                </div>
+                </div>
+                
+            </div>
+            <div class="row fix-row">
+                <div class="col-md-3 fix-size col-xs-3">
+                    <img src="../10/images/3.png" alt="hinh" class="img-responsive">
+                </div>
+                <div class="col-md-9 fix-size1 col-xs-9">
+                <span class="sub-title">What to Wear to a Business Par…</span>
+                <div class="des">
+                enim id volutpat interdum etiam placerat velit vitae dui blandit
+                </div>
+                </div>
+                
+            </div>
+            <div class="row fix-row">
+                <div class="col-md-3 fix-size col-xs-3">
+                    <img src="../10/images/1.png" alt="hinh" class="img-responsive" >
+                </div>
+                <div class="col-md-9 fix-size1 col-xs-9">
+                <span class="sub-title">Protect Your Skin Against Wint…</span>
+                <div class="des">
+                Vulputate justo euismod ac etiam placerat velit vitae dui
+                </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ modules/7/module7-content.php
@@ -0,0 +1,95 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-139">
+    <div class="container">
+        <div class="title-shop"><h1>Shop</h1></div>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="results-count-wrapper">
+                        <p class="result-count-showing">
+                            Showing 1–12 of 33 results	</p>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <form class="select-sort" method="get">
+                        <select name="orderby" class="orderby">
+                                <option value="popularity">Sort by popularity</option>
+                                <option value="rating">Sort by average rating</option>
+                                <option value="date" selected="selected">Sort by latest</option>
+                                <option value="price">Sort by price: low to high</option>
+                                <option value="price-desc">Sort by price: high to low</option>
+                        </select>
+                                
+                        <input type="hidden" name="paged" value="1">
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+            <!--show products-->
+            <div class="col-md-9 col-xs-12 col-sm-8 ">
+                <div class="col-md-4 col-xs-12 col-sm-6" >
+                    <div class="product">
+                        <a href="#" class="img-product"><img src="./images/1.jpg" alt="" class="img-responsive"></a>
+                        <a href="#" class="button-product">Add to cart</a>
+                        <a href="#" class="product-title">
+                            <h2>White Summer Dress</h2>
+                        </a>
+                        <span>$80.00</span>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12 col-sm-6" >
+                    <div class="product">
+                        <a href="#" class="img-product"><img src="./images/2.jpg" alt="" class="img-responsive"></a>
+                        <a href="#" class="button-product">Add to cart</a>
+                        <a href="#" class="product-title">
+                            <h2>White Summer Dress</h2>
+                        </a>
+                        <span>$80.00</span>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12 col-sm-6" >
+                    <div class="product">
+                        <a href="#" class="img-product"><img src="./images/3.jpg" alt="" class="img-responsive"></a>
+                        <a href="#" class="button-product">Add to cart</a>
+                        <a href="#" class="product-title">
+                            <h2>White Summer Dress</h2>
+                        </a>
+                        <span>$80.00</span>
+                    </div>
+                </div>
+            </div> 
+        <div class="col-md-3 col-sm-3 "></div> 
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-9 pages-select">
+                <nav aria-label="Page navigation">
+                  <ul class="pagination">
+                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item">
+                      <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">→</span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+            </div>      
+        </div>
+    </div>   
+</div>
